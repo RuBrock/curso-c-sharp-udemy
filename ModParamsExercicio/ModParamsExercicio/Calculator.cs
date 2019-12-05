db1fff9ec50e95afbec7f104893065c6fd3fa9ab
RuBrock <rubinhobs99@gmail.com>
2019-12-05 20:26:06 -0300

    Modificador Params

---
+++ ModParamsExercicio/ModParamsExercicio/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModParamsExercicio {
+    class Calculator {
+
+        //PARAMS
+        /*Deixa claro para a função que será recebido uma quantia variável de valores*/
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for(int i=0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+    }
+}
---
+++ ModParamsExercicio/ModParamsExercicio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ModParamsExercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            //int result = Calculator.Sum(new int[] { 10, 20, 30, 40 });
+            int result = Calculator.Sum(10, 20, 30);
+
+            Console.WriteLine(result);
+        }
+    }
+}
