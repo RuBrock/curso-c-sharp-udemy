19a1e32a69acec774b2ec89cce24f454e044a5bc
RuBrock <rubinhobs99@gmail.com>
2020-01-06 19:02:15 -0300

    Exercicios novos

---
+++ ForEachExercicio/ForEachExercicio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForEachExercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+            
+            /*for(int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }*/
+
+            foreach(string obj in vect) {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
