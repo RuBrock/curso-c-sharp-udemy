687c385ccfdaf29fc4b25f383865da35b3af0282
RuBrock <rubinhobs99@gmail.com>
2020-02-27 20:57:59 -0300

    Conceitos de Heranca - C#

---
+++ HerancaConcepts/HerancaConcepts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HerancaConcepts.Entities;
+
+namespace HerancaConcepts {
+    class Program {
+        static void Main(string[] args) {
+
+            BusinessAccount account = new BusinessAccount(8010, "Jack Ryan", 100.00, 500.00);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.00;
+
+        }
+    }
+}
